7e7059db24aa50a9abff5024b16275aff56c817d
jorgegarciadelanavarra <jorgegarciadelanavarra@gmail.com>
2023-02-18 11:46:49 +0100

    added login

--- app.module.ts
+++ app.module.ts
@@ -9,9 +9,17 @@ import { ItemService } from './item.service';
 import { CreateItemComponent } from './src/app/create-item/create-item.component';
 import { FormsModule } from '@angular/forms';
 import { ModifyItemComponent } from './src/app/modify-item/modify-item.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from './src/environment';
 @NgModule({
   declarations: [AppComponent, ItemListComponent, CreateItemComponent, ModifyItemComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule, 
+    HttpClientModule, 
+    AngularFireModule.initializeApp(environment.firebaseConfig), 
+    AngularFireAuthModule],
   providers: [ItemService],
   bootstrap: [AppComponent],
 })
